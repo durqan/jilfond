2724a55057bc8e65de501b02dc179446364ee6ba
durqan <duircianos@yandex.ru>
2023-07-01 12:13:59 +0400

    added logout component

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -1,7 +1,12 @@
 @extends('index')
 
 @section('home_page')
-<div>
-    Домашная страница для авторизованных пользователей!
-</div>
+    <div style="display: flex; justify-content: space-between">
+        <div>
+            Домашная страница для авторизованных пользователей!
+        </div>
+        <form action="logout">
+            <button type="submit" class="btn btn-primary">Выйти</button>
+        </form>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorizeController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ use App\Http\Controllers\HomePageController;
 
 Route::get('/', [AuthorizeController::class, 'authorize_form'])->name('login');
 Route::get('/home_page', [HomePageController::class, 'home_page'])->middleware('auth');
+Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
 Route::get('/reg', [RegistrationController::class, 'registration_form']);
 Route::post('/do_reg', [RegistrationController::class, 'action_registration']);
 Route::post('/authorize', [AuthorizeController::class, 'action_authorization']);
